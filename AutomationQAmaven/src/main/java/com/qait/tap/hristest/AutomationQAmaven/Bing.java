86a1cd3db51e431c36911a3ccbba23569bbf9fa5
Chetan Dubey <40267531+chetandubey@users.noreply.github.com>
2018-07-06 15:35:39 +0530

    Update Bing.java

--- AutomationQAmaven/src/main/java/com/qait/tap/hristest/AutomationQAmaven/Bing.java
+++ AutomationQAmaven/src/main/java/com/qait/tap/hristest/AutomationQAmaven/Bing.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.Select;
 
-public class bING
+public class Bing
 {
 	static WebDriver driver;
 	String translatedText;
